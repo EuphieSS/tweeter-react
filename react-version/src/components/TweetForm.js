af3f7ebd549bd61e21bf5a41bc8e4e53559bf9fc
--replace-all <samma118@hotmail.com>
2023-02-18 19:32:38 -0500

    Add event handling

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -3,9 +3,17 @@ function TweetForm() {
 
   return (
     <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
+      <form method="post" action="/tweets" className="newtweet__form" 
+        onSubmit={(Event) =>{
+          Event.preventDefault();
+        }}
+      >
         <textarea className="form__textarea" name="text" placeholder={textAreaMessage}></textarea>
-        <input type="submit" value="Tweet" className="form__input" />
+        <input type="submit" value="Tweet" className="form__input" 
+          onClick={(Event) => {
+            Event.preventDefault();
+          }} 
+        />
         <span className="form__counter">140</span>
       </form>
     </section>
