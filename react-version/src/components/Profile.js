9464c89a09d2052d930aed5bf9aec93f701bf36b
--replace-all <samma118@hotmail.com>
2023-02-18 19:09:40 -0500

    Refactor to convert dynamic data into variables

--- react-version/src/components/Profile.js
+++ react-version/src/components/Profile.js
@@ -1,15 +1,18 @@
-// import ProfilePicture from '../../public/profile-hex.png';
 import ProfilePicture from '../profile-hex.png';
 
 function Profile() {
+  const firstName = "Amy";
+  const lastName = "Mansel";
+  const avatar = ProfilePicture;
+
   return (
     <aside>
       <div className="profile">
-        <img className="profile__image" src={ProfilePicture} alt="Profile" />
+        <img className="profile__image" src={avatar} alt="Profile" />
       </div>
       <br />
       <div className="profile__name">
-        <h2><span className="profile--bold">Amy</span> Mansell</h2>
+        <h2><span className="profile--bold">{firstName}</span> {lastName}</h2>
       </div>
     </aside>
   );
